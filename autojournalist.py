6ef89a6d0d2211fbbfe2873cc3c61e3682f4d91c
Greg Baker <gregb@ifost.org.au>
2022-01-01 11:48:49 +1100

    One day this will do more.

---
+++ autojournalist.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# This doesn't do much at the moment.
+
+import time
+today = time.strftime("%Y-%m-%d")
+
+with open('output/README.md', 'w') as markdown:
+  markdown.write(f"""# NSW Covid Update for {today}
+
+!()[{today}.png]
+""")
+
