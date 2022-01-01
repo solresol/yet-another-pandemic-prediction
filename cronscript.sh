9f3f323c6b1dd1d7ca0661a068816fd6cfaa0d1c
Greg Baker <gregb@ifost.org.au>
2022-01-01 11:49:19 +1100

    Run the auto journalist

--- cronscript.sh
+++ cronscript.sh
@@ -4,7 +4,8 @@ cd $(dirname $0)
 git pull -q
 mkdir -p output
 ./projection.py $*
-git add output/*.png
+./autojournalist.py $*
+git add output/*.png output/README.md
 TODAY=$(date +%Y-%B-%d)
 git commit -q -m"Output as of $TODAY"
 git push -q
