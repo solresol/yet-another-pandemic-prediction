f00923fbfa61d3d937e642d8059380f7b6368fd7
Greg Baker <gregb@ifost.org.au>
2022-01-03 14:57:13 +1100

    Make separate images.

--- projection.py
+++ projection.py
@@ -8,10 +8,10 @@ import numpy
 import argparse
 import os
 import datamgmt
+import time
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--copy-to", help="Output the image into another directory as well")
-args = parser.parse_args()
+today = time.strftime("%Y-%m-%d")
+os.makedirs(f'output/{today}', exist_ok=True)
 
 omicron_cases, omicron_tests, omicron_hospital, omicron_deaths = datamgmt.fetch_covidlive()
 
@@ -20,7 +20,7 @@ time_data['when_tstamp'] = time_data.when.view('int64') / 1e9 / 86400
 
 def make_model(dataset):
     time_data = pandas.DataFrame({'when': dataset.index})
-    time_data['when_tstamp'] = time_data.when.view('int64') / 1e9 / 86400    
+    time_data['when_tstamp'] = time_data.when.view('int64') / 1e9 / 86400
     log_dataset = dataset.map(math.log10)
     model = sklearn.linear_model.TheilSenRegressor()
     model.fit(time_data[['when_tstamp']], log_dataset)
@@ -28,13 +28,13 @@ def make_model(dataset):
 
 def denoise_and_extrapolate(dataset, model, number_of_days=90):
     time_data = pandas.DataFrame({'when': dataset.index})
-    time_data['when_tstamp'] = time_data.when.view('int64') / 1e9 / 86400        
+    time_data['when_tstamp'] = time_data.when.view('int64') / 1e9 / 86400
     future = pandas.DataFrame({'when':
                            [time_data.when.max() + pandas.to_timedelta(f'{n}d') for n in range(1,number_of_days)]})
     future['when_tstamp'] = future.when.view('int64') / 1e9/ 86400
-    model_test = pandas.Series(data=model.predict(time_data[['when_tstamp']]), 
+    model_test = pandas.Series(data=model.predict(time_data[['when_tstamp']]),
                                 index=time_data.when).map(lambda x: 10**x)
-    model_extrapolate = pandas.Series(data=model.predict(future[['when_tstamp']]), 
+    model_extrapolate = pandas.Series(data=model.predict(future[['when_tstamp']]),
                                    index=future.when).map(lambda x: 10**x)
     return model_test, model_extrapolate
 
@@ -53,33 +53,53 @@ def make_exponential_plot(dataset, title, ax, log_plot=True):
     ax.legend(loc='lower right')
 
 
+h = datamgmt.History()
 
-fig, axes = matplotlib.pyplot.subplots(nrows=5, figsize=(12,20))
-make_exponential_plot(omicron_cases.CASES/1e6, "Number of people who have been infected in NSW (millions)",
-                      axes[0], log_plot=False)
-axes[0].axhline(8.16, color="red")
-axes[0].annotate(xy=(time_data.when.min(), 7.8), s="Population of NSW", color='red')
+with matplotlib.pyplot.style.context('seaborn-darkgrid'):
+    fig, ax = matplotlib.pyplot.subplots(figsize=(12,5))
+    dataset = omicron_cases.CASES/1e6
+    make_exponential_plot(dataset, "Number of people who have been infected in NSW (millions)", ax, log_plot=False)
+    ax.axhline(8.16, color="red")
+    ax.annotate(xy=(time_data.when.min(), 7.8), s="Population of NSW", color='red')
+    model = make_model(dataset)
+    h.add_doubling_rate_to_history('infection', today, doubling_period_of_model(model))
+    fig.savefig(f"output/{today}/infection.png")
 
-make_exponential_plot(omicron_hospital.HOSP, "Number of covid-19 patients in hospital (log scale)", ax=axes[1])
-axes[1].axhline(20000, color="red")
-axes[1].annotate(xy=(time_data.when.min(), 22000), s="Number of hospital beds in NSW", color='red')
+with matplotlib.pyplot.style.context('seaborn-darkgrid'):
+    fig, ax = matplotlib.pyplot.subplots(figsize=(12,5))
+    dataset = omicron_hospital.HOSP
+    make_exponential_plot(dataset, "Number of covid-19 patients in hospital (log scale)", ax=ax)
+    ax.axhline(20000, color="red")
+    ax.annotate(xy=(time_data.when.min(), 22000), s="Number of hospital beds in NSW", color='red')
+    model = make_model(dataset)
+    h.add_doubling_rate_to_history('hospitalisation', today, doubling_period_of_model(model))
+    fig.savefig(f"output/{today}/hospitalisation.png")
 
+with matplotlib.pyplot.style.context('seaborn-darkgrid'):
+    fig, ax = matplotlib.pyplot.subplots(figsize=(12,5))
+    dataset = omicron_hospital.ICU
+    make_exponential_plot(dataset, "Number of covid-19 patients in ICU (log scale)",  ax=ax)
+    ax.axhline(500, color='red')
+    ax.axhline(2000, color='red')
+    ax.annotate(xy=(time_data.when.min(),550), s="Basic ICU capacity", color='red')
+    ax.annotate(xy=(time_data.when.min(),2050), s="Maximum ICU capacity (surge)", color='red')
+    model = make_model(dataset)
+    h.add_doubling_rate_to_history('icu', today, doubling_period_of_model(model))
+    fig.savefig(f"output/{today}/icu.png")
 
-make_exponential_plot(omicron_hospital.ICU, "Number of covid-19 patients in ICU (log scale)",  ax=axes[2])
-axes[2].axhline(500, color='red')
-axes[2].axhline(2000, color='red')
-axes[2].annotate(xy=(time_data.when.min(),550), s="Basic ICU capacity", color='red')
-axes[2].annotate(xy=(time_data.when.min(),2050), s="Maximum ICU capacity (surge)", color='red')
+with matplotlib.pyplot.style.context('seaborn-darkgrid'):
+    fig, ax = matplotlib.pyplot.subplots(figsize=(12,5))
+    dataset = omicron_hospital.VENT
+    make_exponential_plot(dataset, "Number of covid-19 patients on ventilators", ax=ax, log_plot=False)
+    model = make_model(dataset)
+    h.add_doubling_rate_to_history('ventilators', today, doubling_period_of_model(model))
+    fig.savefig(f"output/{today}/ventilators.png")
 
-make_exponential_plot(omicron_hospital.VENT, "Number of covid-19 patients on ventilators", ax=axes[3], log_plot=False)
-
-make_exponential_plot(omicron_deaths.DEATHS, "Number of covid-19 related deaths", ax=axes[4], log_plot=False)
-axes[4].set_ylim(0,1000)
-fig.tight_layout()
-date_of_prediction = omicron_cases.index.max().date()
-if args.copy_to is not None:
-   filename = os.path.join(args.copy_to, f"{date_of_prediction}.png")
-   fig.savefig(filename)
-
-filename = os.path.join('output', f"{date_of_prediction}.png")
-fig.savefig(filename)
+with matplotlib.pyplot.style.context('seaborn-darkgrid'):
+    fig, ax = matplotlib.pyplot.subplots(figsize=(12,5))
+    dataset = omicron_deaths.DEATHS
+    make_exponential_plot(dataset, "Number of covid-19 related deaths", ax=ax, log_plot=False)
+    ax.set_ylim(0,1000)
+    model = make_model(dataset)
+    h.add_doubling_rate_to_history('deaths', today, doubling_period_of_model(model))
+    fig.savefig(f"output/{today}/deaths.png")
