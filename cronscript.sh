1b82874d48a0edd0d9e90434b263a46a124898fa
Greg Baker <gregb@ifost.org.au>
2022-01-23 12:42:45 +1100

    We don't have historical data any more.

--- cronscript.sh
+++ cronscript.sh
@@ -9,7 +9,7 @@ TODAY_WORDS=$(date +%Y-%B-%d)
 TODAY=$(date +%Y-%m-%d)
 (cd output ; pandoc -f markdown -t docx -o ${TODAY}/nsw-covid-report-${TODAY}.docx README.md )
 (cd output ; pandoc -f markdown -t latex --toc -o ${TODAY}/nsw-covid-report-${TODAY}.pdf README.md )
-git add output/${TODAY}/*.png output/${TODAY}/historical/*.png output/${TODAY}/nsw-covid-report-${TODAY}.docx output/${TODAY}/nsw-covid-report-${TODAY}.pdf output/README.md
+git add output/${TODAY}/*.png output/${TODAY}/nsw-covid-report-${TODAY}.docx output/${TODAY}/nsw-covid-report-${TODAY}.pdf output/README.md
 git commit -q -m"Output as of $TODAY_WORDS"
 git add historical/*.csv
 git commit -q -m"Record the predictions as of ${TODAY_WORDS}"
